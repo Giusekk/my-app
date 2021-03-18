9612418b8b353b94642ca93fd4c36abfc90da395
Giusekk <mannargiusepp@gmail.com>
2021-03-18 18:20:34 +0100

    Updated app site.

--- src/App.js
+++ src/App.js
@@ -20,31 +20,28 @@ import Main from './Layout/Homepage/Main/Main.js';
 import Heading from './Layout/Homepage/Heading/Heading.js';
 import ViewArticle from "./Layout/Homepage/ViewArticle/ViewArticle";
 import NewArticle from "./Layout/Homepage/NewArticle/NewArticle";
-import { Home } from "@material-ui/icons";
 
 function App() {
   set_mobile_theme();
 
   return (
     <Div>
-       <Router basename={process.env.PUBLIC_URL}>
+       <Router >
          <Heading />
          <Switch>
-           {/*<Route path="/my-app" component={NewArticle} exact>
-             <Main />
+           <Route path="/my-app" component={NewArticle} exact>
+             {/*<Main />*/}
              <NewArticle />
            </Route>
            <Route exact path="/my-app/article/:id">
              <ViewArticle />
            </Route>
-           <Route component={NewArticle} path="new-article">
+           <Route path="/my-app/new-article">
              <NewArticle />
            </Route>
            <Route path="/my-app/d">
              <p>sada</p>
-            </Route>*/}
-            <Route exact path="/" component={Main} />
-            <Route exact path="/new-article" component={NewArticle} />
+           </Route>
          </Switch>
        </Router>
        <a href="/my-app/d">Ciao</a>
